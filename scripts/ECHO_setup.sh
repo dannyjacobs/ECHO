1d4274727c961b27ae9053af766c87fc2c275500
Jacob Burba <jburba@asu.edu>
2016-01-11 12:51:30 -0700

    Update ECHO_setup.sh

--- scripts/ECHO_setup.sh
+++ scripts/ECHO_setup.sh
@@ -6,11 +6,12 @@ read port
 echo -n "Enter Baud rate: "
 read baud
 
+echo -n "Enter APM Planner 2.0 location: "
+read apm_loc
+
 mavproxy.py --master=/dev/tty.$port --baudrate=$baud --out=udp.127.0.0.1:14550 --out=udp.127.0.0.1:14551 &
 pid1 = $!
 
-sleep10 ;
+sleep 5 ;
 
-open /Applications/APM\ Planner\ 2.0.app/
-
-#wait $pid1
+open $apm_loc/APM\ Planner\ 2.0.app/
