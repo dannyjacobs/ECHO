9e8d9d1408facba3347bdc775d38dae2db0a792d
Jacob Burba <jburba@asu.edu>
2016-05-03 13:30:54 -0700

    Revised setup script

--- scripts/ECHO_setup.sh
+++ scripts/ECHO_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -n "Enter MAVProxy connection port: (i.e. /dev/tty.usb*)"
+echo -n "Enter MAVProxy connection port (i.e. /dev/tty.usb*): "
 read port
 
-echo -n "Enter Baud rate: (57600 for 3DR radio)"
+echo -n "Enter Baud rate: (57600 for 3DR radio): "
 read baud
 
-echo -n "Enter APM Planner 2.0 location: (i.e. /Applications) "
+echo -n "Enter APM Planner 2.0 location: (i.e. /Applications): "
 read apm_loc
 
 open $apm_loc/APM\ Planner\ 2.0.app/
