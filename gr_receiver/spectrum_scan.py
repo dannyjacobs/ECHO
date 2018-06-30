4091b3a6c4f6cd2ccf026bfaa95f0a334a0f598b
Daniel Jacobs <daniel.c.jacobs@asu.edu>
2018-06-29 18:11:56 -0700

    scanning spectrometer works on x300!

--- gr_receiver/spectrum_scan.py
+++ gr_receiver/spectrum_scan.py
@@ -14,16 +14,22 @@ import sys
 import time
 import zmq
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+def round_to_nearest_2(x):
+    return 2**(x-1).bit_length()
+
 
 def zmq_consumer(socket_str):
-    #input: URL and port (ex: tcp://127.0.0.1:5555
+    #input: URL and port (ex: "tcp://127.0.0.1:5555")
     #get data, make an array and do something with it
     context = zmq.Context()
     results_receiver = context.socket(zmq.PULL)
     results_receiver.connect(socket_str)
     raw_data = results_receiver.recv()
     data = np.frombuffer(raw_data,dtype=np.complex64)
-    print(data.size/1024.) #i'm expecting 1024 complex numbers
+    return np.array(data)
 
 #given a range of frequencies and resolution
 # divide the range into discrete chunks
@@ -40,33 +46,47 @@ def zmq_consumer(socket_str):
 
 # suppose we wanted 2GHz of total bw
 B = 2e9
+df = 100e3 #desired spectral resolution 100kHz would be reasonable for HERA
+
+from spectrum import spectrum
+
+#SDR parameters
+SDR_BW = 160e6
 # and we knew that we had 
-SDR_BW = 160e6 #UBX160 is what we have in Andri's X300s
 #you can find out by running uhd_usrp_probe
 nchunk = np.ceil(B/SDR_BW)
-df = 100e3 #desired spectral resolution 100kHz would be reasonable for HERA
-FFT_size = np.ceil(SDR_BW/df)
+FFT_size = int(np.ceil(SDR_BW/df))
+FFT_size = round_to_nearest_2(FFT_size)
 print("Scan Bandwidth [MHz]= ",B/1.e6)
-print("SDR Bandwidth [kHz] =" ,SDR_BW/1.e3)
+print("SDR Bandwidth [MHz] =" ,SDR_BW/1.e6)
 print("Number of chunks = ",nchunk)
 print("FFT_size = ",FFT_size)
 tunings = np.arange(nchunk)*SDR_BW + SDR_BW/2.
-
-from spectrum import spectrum
+nchan = nchunk*FFT_size
+print("total number of channels in spectrum = ",nchan)
+data = np.zeros(nchan)
 
 def main(top_block_cls=spectrum,options=None):
     qapp = Qt.QApplication(sys.argv)
 
-    tb = top_block_cls()
+    tb = top_block_cls(FFT_size,SDR_BW)
     socket_str = tb.get_data_address() #data are streaming from this ZMQ PUSH Sink
-    for freq in tunings:#get chunks at each spectrum
-        print("="*100)
+    for i,freq in enumerate(tunings):#get chunks at each spectrum
+        print(i,freq)
 	tb.set_tuning(freq)
         tb.start() 
-        zmq_consumer(socket_str)
-	#time.sleep(0.01)
+        data_buffer = zmq_consumer(socket_str) #data should be NchanxNtimes long where Ntimes floats around but is generally 2-6
+        Ntimes = data_buffer.size/FFT_size
+        data_buffer.shape = (Ntimes,FFT_size) #its either this order or the other one
+        data_buffer = np.abs(data_buffer) #power spectrum
+        data_buffer_mean = np.mean(data_buffer,axis=0)
+        data[i*FFT_size:(i+1)*FFT_size] = data_buffer_mean
         tb.stop()
         tb.wait()
+    freqs = np.linspace(0,np.ceil(B/df),num=len(data))*df
+    plt.plot(freqs/1e6,10*np.log10(data))
+    #plt.plot(data)
+    plt.show()
 
 if __name__ == '__main__':
     main()
