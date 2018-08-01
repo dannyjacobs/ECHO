82c13a211c92b86e0e315a28e573ced8221f9c73
Mickey Horn <mrhorn1@asu.edu>
2018-08-01 12:24:21 -0700

    Rewrite of plot_slices.py to make it more general
    Still want to add command line input for compared model

--- scripts/plot_slices.py
+++ scripts/plot_slices.py
@@ -15,7 +15,7 @@ o.add_option('--exp',action='store_true',help='do experimental stuff')
 opts,args = o.parse_args(sys.argv[1:])
 
 base_model = '/home/echo/Downloads/BicoLOG_results_ff137.fits'
-vader_model = '/home/echo/Downloads/Vader_results_ff137.fits'
+compared_model = '/home/echo/Downloads/BicoLog_anker_valon_ff137.fits'
 
 
 #Try a short dipole model.
@@ -31,11 +31,11 @@ base_map -= np.mean(base_map[:3])
 base_slice_E = get_interp_val(base_map,alt,az)
 base_slice_H = get_interp_val(base_map,alt,az+np.pi/2)
 
-#get the vader model
-vader_map = read_map(vader_model)
-vader_map -= np.mean(base_map[:3])
-vader_slice_E = get_interp_val(vader_map,alt,az)
-vader_slice_H = get_interp_val(vader_map,alt,az+np.pi/2)
+#get the model to compare to
+compared_map = read_map(compared_model)
+compared_map -= np.mean(compared_map[:3])
+compared_slice_E = get_interp_val(compared_map,alt,az)
+compared_slice_H = get_interp_val(compared_map,alt,az+np.pi/2)
 
 #altfig = figure(figsize=(8,6))
 fig,axarr = subplots(2,3,figsize=(15,6),sharey=False,sharex=True)
@@ -46,15 +46,15 @@ ls = ['s','d','d','s'] #file order is north, south, south, north
 
 #plot the base model
 axarr[0,0].plot(alt*180/np.pi,base_slice_E,'-k',lw=2)
-axarr[0,1].plot(alt*180/np.pi,vader_slice_E,'-k',lw=2)
-axarr[0,2].plot(alt*180/np.pi,base_slice_E-vader_slice_E,'-k',lw=2)
+axarr[0,1].plot(alt*180/np.pi,compared_slice_E,'-k',lw=2)
+axarr[0,2].plot(alt*180/np.pi,base_slice_E-compared_slice_E,'-k',lw=2)
 axarr[1,0].plot(alt*180/np.pi,base_slice_H,'-k',lw=2)
-axarr[1,1].plot(alt*180/np.pi,vader_slice_H,'-k',lw=2)
-axarr[1,2].plot(alt*180/np.pi,base_slice_H-vader_slice_H,'-k',lw=2)
+axarr[1,1].plot(alt*180/np.pi,compared_slice_H,'-k',lw=2)
+axarr[1,2].plot(alt*180/np.pi,base_slice_H-compared_slice_H,'-k',lw=2)
 #legend(loc='best')
 axarr[0,0].set_title('Base')
-axarr[0,1].set_title('Vader')
-axarr[0,2].set_title('Base - Vader')
+axarr[0,1].set_title('Compared')
+axarr[0,2].set_title('Base - Compared')
 axarr[0,0].set_ylabel('E plane\n [dB V/m]')
 axarr[1,0].set_ylabel('H plane\n [dB V/m]')
 axarr[1,0].set_xlabel('$\\theta$ (deg)')
