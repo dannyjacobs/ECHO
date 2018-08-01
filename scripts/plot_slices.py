f56ed1ceb172c5d9fe88a918cecbebf8ff6093a9
Mickey Horn <mrhorn1@asu.edu>
2018-08-01 15:36:40 -0700

    Edits to plot_slices.py to take in a test file as an input from the command line

--- scripts/plot_slices.py
+++ scripts/plot_slices.py
@@ -14,8 +14,8 @@ o.add_option('--savefig',type=str,help='output figure name')
 o.add_option('--exp',action='store_true',help='do experimental stuff')
 opts,args = o.parse_args(sys.argv[1:])
 
-base_model = '/home/echo/Downloads/BicoLOG_results_ff137.fits'
-compared_model = '/home/echo/Downloads/BicoLog_anker_valon_ff137.fits'
+base_model = '/home/echo/Downloads/results_BicoLOG_ff137.fits'
+test_model = args[0]
 
 
 #Try a short dipole model.
@@ -31,11 +31,11 @@ base_map -= np.mean(base_map[:3])
 base_slice_E = get_interp_val(base_map,alt,az)
 base_slice_H = get_interp_val(base_map,alt,az+np.pi/2)
 
-#get the model to compare to
-compared_map = read_map(compared_model)
-compared_map -= np.mean(compared_map[:3])
-compared_slice_E = get_interp_val(compared_map,alt,az)
-compared_slice_H = get_interp_val(compared_map,alt,az+np.pi/2)
+#get the test model
+test_map = read_map(test_model)
+test_map -= np.mean(test_map[:3])
+test_slice_E = get_interp_val(test_map,alt,az)
+test_slice_H = get_interp_val(test_map,alt,az+np.pi/2)
 
 #altfig = figure(figsize=(8,6))
 fig,axarr = subplots(2,3,figsize=(15,6),sharey=False,sharex=True)
@@ -44,17 +44,17 @@ E_color = 'k'
 H_color = 'k'
 ls = ['s','d','d','s'] #file order is north, south, south, north
 
-#plot the base model
+#plot
 axarr[0,0].plot(alt*180/np.pi,base_slice_E,'-k',lw=2)
-axarr[0,1].plot(alt*180/np.pi,compared_slice_E,'-k',lw=2)
-axarr[0,2].plot(alt*180/np.pi,base_slice_E-compared_slice_E,'-k',lw=2)
+axarr[0,1].plot(alt*180/np.pi,test_slice_E,'-k',lw=2)
+axarr[0,2].plot(alt*180/np.pi,base_slice_E-test_slice_E,'-k',lw=2)
 axarr[1,0].plot(alt*180/np.pi,base_slice_H,'-k',lw=2)
-axarr[1,1].plot(alt*180/np.pi,compared_slice_H,'-k',lw=2)
-axarr[1,2].plot(alt*180/np.pi,base_slice_H-compared_slice_H,'-k',lw=2)
+axarr[1,1].plot(alt*180/np.pi,test_slice_H,'-k',lw=2)
+axarr[1,2].plot(alt*180/np.pi,base_slice_H-test_slice_H,'-k',lw=2)
 #legend(loc='best')
 axarr[0,0].set_title('Base')
-axarr[0,1].set_title('Compared')
-axarr[0,2].set_title('Base - Compared')
+axarr[0,1].set_title('Test')
+axarr[0,2].set_title('Base - Test')
 axarr[0,0].set_ylabel('E plane\n [dB V/m]')
 axarr[1,0].set_ylabel('H plane\n [dB V/m]')
 axarr[1,0].set_xlabel('$\\theta$ (deg)')
@@ -62,12 +62,12 @@ axarr[1,1].set_xlabel('$\\theta$ (deg)')
 axarr[1,2].set_xlabel('$\\theta$ (deg)')
 #axes
 subplots_adjust(hspace=.1,wspace=.2)
-axarr[0,0].set_ylim(-20,2)
-axarr[0,1].set_ylim(-20,2)
-axarr[0,2].set_ylim(-3,3)
-axarr[1,0].set_ylim(-10,2)
-axarr[1,1].set_ylim(-10,2)
-axarr[1,2].set_ylim(-3,3)
+axarr[0,0].autoscale(enable=True, axis='y', tight=True)
+axarr[0,1].autoscale(enable=True, axis='y', tight=True)
+axarr[0,2].autoscale(enable=True, axis='y', tight=True)
+axarr[1,0].autoscale(enable=True, axis='y', tight=True)
+axarr[1,1].autoscale(enable=True, axis='y', tight=True)
+axarr[1,2].autoscale(enable=True, axis='y', tight=True)
 for ax in axarr.ravel():
     ax.grid(which='both') #turn on everyones grid
     ax.xaxis.set_ticks(np.arange(-80,100,20))
