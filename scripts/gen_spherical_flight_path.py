86541ff84fa41cee483a04bdce54e18e5761da99
Mickey Horn <mickeyhorn7@gmail.com>
2019-10-03 15:08:50 -0700

    old code clean up, added defaults
    removed old code that is no longer being used in the script and added defaults to most options

--- scripts/gen_spherical_flight_path.py
+++ scripts/gen_spherical_flight_path.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
-import numpy as n,sys,os
+import numpy as n,sys
 import optparse, healpy
 from pylab import *
-from flight_planning import print_MAV_WPT,print_MAV_YAW,APM_PLANNER_FILE_HEADER,R_earth,length
+from flight_planning import print_MAV_WPT,APM_PLANNER_FILE_HEADER,R_earth,length
 """
 Input some basic parameters to
 
@@ -13,59 +13,38 @@ Output a new waypoint file for a spherical flight with waypoints on a healpix gr
 
 o = optparse.OptionParser()
 o.set_description('''Generates a waypoint file for a series of concentric circles stacked to make constant _distance_ flight.''')
-o.add_option('--radius',type=float,
+o.add_option('--radius',type=float,default=100,
     help='distance from antenna under test [m]')
-#o.add_option('--norbits',type=int,
-#    help='number of circles to execute')
-o.add_option('--min_height',type=float,
+o.add_option('--min_height',type=float,default=6,
     help='the base level of the lowest pass [m]')
 o.add_option('--center',type=str,
    help='lat_lon of antenna under test in degrees [REQUIRED]')
-#o.add_option('--delta_el',type=float,
-#    help='angular elevation seperation between flight levels [deg]')
-#o.add_option('--delta_az',type=float,
-#    help='angular azimuth separation between circle waypoints [deg]')
-o.add_option('--velocity',type=float,
+o.add_option('--velocity',type=float,default=2,
     help='WPT_NAV velocity setting in m/s')
 o.add_option('--file_prefix',type=str,
     help='output filename')
-o.add_option('--sortie_length',type=float,
+o.add_option('--sortie_length',type=float,default=30,
     help='break the total pattern into sorties of this length [minutes]')
-o.add_option('--nside',type=int,
+o.add_option('--nside',type=int,default=8,
     help='the nside of the desired healpix point grid')
-o.add_option('--max_points',type=int,default=75,
-    help='max number of points to put in the file. apm planner 2 gets unreliable above 80. default=75')
+o.add_option('--max_points',type=int,default=500,
+    help='max number of points to put in the file. apm planner 2 got unreliable above 80.')
 o.add_option('--pol_angle',type=float,default=0,
     help='angle of dipole wrt north [deg]')
-#o.add_option('--stripe_spacing',type=float,default=10,
-#    help=' Default is 10m')
-#o.add_option('--nstripes',type=int,
-#    help='number of stripes [REQUIRED]')
-#o.add_option('--length',type=float,
-#    help='Length of stripes in meters [REQUIRED]')
-#o.add_option('--alt',type=float,
-#    help='alt in meters [REQUIRED]')
 o.add_option('--yaw_angle',type=float,default=0,
-             help='angle the drone is point wrt north [deg]')
+    help='angle the drone is point wrt north [deg]')
 opts,args = o.parse_args(sys.argv[1:])
 
 #parse inputs
 center_lat,center_lon = map(float,opts.center.split('_'))
 
-#RADIAN conversion!!!
-#DELTA_ELEVATION = opts.delta_el*n.pi/180
-#DELTA_AZIMUTH=opts.delta_az*n.pi/180
-# ------ below this point everything is in RADIANS ------
-# -----  (we'll print everything in degrees at the end) --
-
-
-
+# ------ below this point everything is in RADIANS ----------
+# -----  (we'll print everything in degrees at the end) -----
 pixnums = n.arange(healpy.nside2npix(opts.nside)) #nside2npix=3072 when nside=16, so array from 0 to 3071
 co_els,azimuths = healpy.pix2ang(opts.nside,pixnums,nest=False) #colatitude and longitude in radians
 elevations = n.pi/2 - co_els #converts colatitude to latitude
 #elevations go from pi at the north pole to -pi at the south pole
 #azimuths go from 0 due north to 2pi all the way around
-
 
 print "computing elevation coverage"
 min_el = opts.min_height/opts.radius #typically 6/100
@@ -77,23 +56,16 @@ print "Generating healpix grid with {n} points".format(n=len(azimuths))
 print "min elevation = {min}, max elevation = {max}".format(
     min=elevations.min()*180/n.pi,max=elevations.max()*180/n.pi)
 
-altitude_levels = n.sin(elevations)*opts.radius
+altitude_levels = n.sin(elevations)*opts.radius #converts elevations into altitudes from min_el to radius
 
 print "computing altitudes in range: {min} - {max} meters".format(min=altitude_levels.min(),max=altitude_levels.max())
 coords = []
-#for each circle
-#for i,alt in enumerate(altitude_levels):
-#    el = elevations[i]
-#    for az in azimuths:
-#        coords.append([opts.radius*n.cos(el)*n.cos(az),
-#        opts.radius*n.cos(el)*n.sin(az),
-#        alt])
 X = opts.radius*n.cos(elevations)*n.cos(azimuths) #x coordinate in meters from center
 Y = opts.radius*n.cos(elevations)*n.sin(azimuths) #y coordinate in meters from center
 Z = altitude_levels #z coordinate in altitude
-#coords = n.array(coords)
 coords = n.vstack((X,Y,Z)).T #makes an array of [x,y,z] arrays
 coords = n.flipud(coords) #puts the first waypoint first
+
 print coords.shape
 subplot(211)
 plot(coords[:,0],coords[:,1])
@@ -111,29 +83,20 @@ print "total flight distance = {dist} [m]".format(dist=flight_distance)
 print "estimated flying time at {vel} m/s ={time} [minutes]".format(
             vel=opts.velocity,time=flight_distance//opts.velocity/60)
 
-
-
-#covert to lat, lon in radians
+#convert to lat, lon in radians
 coords[:,:2] /= R_earth
-
 #converting final coordinates to DEGREES
-# from here all units are lat/lon degrees and meters
+#from here all units are lat/lon degrees and meters
 coords[:,:2] *= 180/n.pi
-# add offset to center pattern over target location
+#add offset to center pattern over target location
 coords[:,0] += center_lat
 coords[:,1] += center_lon
-
-
 
 #text file creation
 header_lines = []
 offset = 0
 header_lines.append(APM_PLANNER_FILE_HEADER) #"QGC WPL 110"
-#header_lines.append(current_waypoint.strip())
 offset +=1
-#print pointing (sets the polarization)
-#header_lines.append(str(offset)+'\t'+print_MAV_YAW(center_lat,center_lon,50,angle=opts.pol_angle-90,ROI_distance=5e3)) #not needed with new yaw system?
-#offset +=1 #also not needed if ^^ isn't needed
 header_offset = offset
 
 current_length = 0
