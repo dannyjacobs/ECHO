adb3c55da4ba5509f4e2e8cae3e243500e810708
dmlewis9 <dmlewis9@asu.edu>
2020-01-30 18:25:56 -0700

    More deletes, renamed tests and added folders for each test type, edited setup for tests

--- setup.py
+++ setup.py
@@ -4,16 +4,21 @@ import os.path as op
 
 __version__ = '0.0.1'
 
+
 setup_args = {
     'name': 'ECHO',
     'author': 'ECHO Collaboration',
     'license': 'BSD',
     'package_dir': {'ECHO': 'src'},
     'packages': ['ECHO'],
-    'scripts': glob.glob('scripts/*'),
+    'scripts': glob.glob('scripts/*.py'),
     'version': __version__,
-    'package_data': {'ECHO': [op.join('data', '*')]}
+    'package_data': {'ECHO': [op.join('data', '*')]},
+    'setup_requires': ['pytest-runner', 'numpy'],
+    'install_requires': ['numpy', 'scipy', 'healpy'],
+    'tests_require': ['pytest', 'pytest-cases'],
+
 }
 
 if __name__ == '__main__':
-    apply(setup, (), setup_args)
+    setup(**setup_args)
