d694e77a84a1f24aa36be70d497a3f6a9ebe3083
dmlewis9 <dmlewis9@asu.edu>
2020-03-18 08:37:46 -0700

    Added pyulog dependency for setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup_args = {
     'version': __version__,
     'package_data': {'ECHO': [op.join('data', '*')]},
     'setup_requires': ['pytest-runner', 'numpy'],
-    'install_requires': ['numpy', 'scipy', 'healpy', 'flask'],
+    'install_requires': ['numpy', 'scipy', 'healpy', 'flask', 'pyulog'],
     'tests_require': ['pytest', 'pytest-cases'],
 
 }
