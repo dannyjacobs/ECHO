0318590eab65eaf88a33b2f51699bfc52c1aeec8
mrudula-gopalkrishna <mrudula.gopalkrishna@gmail.com>
2020-08-17 11:38:41 -0700

    rx power dB conversion

--- ECHO/read_utils.py
+++ ECHO/read_utils.py
@@ -386,7 +386,7 @@ def mission_endpoint_flagging(pos_data,wpt_data):
         ulog (int): the ulog to be converted.
 
     Returns:
-        flagged_array: array of flagged data.
+        flagged_data: array of flagged data.
         mission_data: array of valid mission data.
 
     """
@@ -763,7 +763,7 @@ def read_ulog(ulog, output=None, messages='vehicle_global_position,vehicle_local
 
 def read_h5(dataFile):
     """
-    Read in ulog file, put them into appropriate arrays
+    Read in receiver data file, put them into appropriate arrays
 
     Input:
         target_data (HDF5 data file): the datafile for the received power for the target antenna, saved in in h5 format.
@@ -771,6 +771,9 @@ def read_h5(dataFile):
     Output:
         dataDict: A dictionary containing the observation data. Includes observations, tunings, times, XX and YY polarizations, frequencies
     """
+
+    def dB(x):
+        return 10*np.log10(x)
 
     target_data = h5py.File(dataFile,'r')
     keys = [key for key in target_data.keys()]
@@ -789,6 +792,8 @@ def read_h5(dataFile):
                 for tunKey in tuningKeys:
                     #print(key, obsKey, tunKey)
                     data = np.asarray(target_data[key][obsKey][tunKey])
+                    if tunKey == 'XX' or tunKey == 'YY':
+                        data = dB(data)
                     tunDict[tunKey] = data
                 obsDict[obsKey] = tunDict
         dataDict[key] = obsDict
